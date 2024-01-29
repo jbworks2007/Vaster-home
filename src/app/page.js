229848333451680ebbc8380968a3cc3563c4ae71
jbworks2007 <jbworks2007@gmail.com>
2024-01-29 20:32:05 +0530

    update2

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="center">
-        <Link className="btn btn-mango" href="authorize/login">
+        <Link className="btn btn-mango" href="/home">
           JOIN THE MOVEMENT
         </Link>
       </div>
